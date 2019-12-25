ac118f86a9d79dfe976325da992e7cd22bd9c922
Blaze-Lu <59202874+Blaze-Lu@users.noreply.github.com>
2019-12-25 19:51:42 +0800

    正式工程第一次创建
    利用unity内置github

---
+++ Assets/Scripts/ShowName.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowName : MonoBehaviour
+{
+
+    bool isShowInfo;
+    bool isEnable;
+    public GUIStyle _GUIStyle;
+    public float Offset = 15;
+    public string Info = "名字";
+    //public Button m_PlayButton;
+    //Material m_Material;
+    // Use this for initialization
+    public void Start()
+    {
+        isShowInfo = false;
+        isEnable = false;
+
+        // m_Material = GetComponent<Renderer>().material;
+    }
+    void OnMouseEnter()
+    {
+        isShowInfo = true;
+       // m_Material.color = Color.red;
+        // print("onmouseenter");
+    }
+    void OnMouseExit()
+    {
+        isShowInfo = false;
+       // m_Material.color = Color.white;
+        // print("onmouseexit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Click()
+    {
+        isEnable = true;
+        Debug.Log("Button Clicked. TestClick.");
+    }
+
+    void OnGUI()
+    {
+
+        if (isEnable)
+        {
+            if (isShowInfo)
+            {
+                print("123");
+                GUI.Label(new Rect(Input.mousePosition.x + Offset, Screen.height - Input.mousePosition.y, 100, 100), Info, _GUIStyle);
+            }
+        }
+       
+    }
+}
